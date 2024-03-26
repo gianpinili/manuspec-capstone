fe111921f8914ae9cba6b5ba7952902af27ecb0b
Ian Pinili <gianpinili@gmail.com>
2024-03-26 12:44:32 -0700

    update profile page styling

--- client-manuspec/src/pages/CustomersFile/CustomersFile.jsx
+++ client-manuspec/src/pages/CustomersFile/CustomersFile.jsx
@@ -10,7 +10,7 @@ function CustomersFile() {
           <div className="customer-info__image">
             <div className="customer-info__img"></div>
           </div>
-          <p className="customer-info__name">Rick Tocchet</p>
+          <p className="customer-info__name">Christi Suwoo</p>
         </div>
         <div className="customer-info__middle">
           <p className="customer-info__number">604-456-7890</p>
@@ -18,7 +18,7 @@ function CustomersFile() {
         </div>
         <div className="customer-info__right">
           <p className="customer-info__vin">1FAHP3F20CL266328</p>
-          <p className="customer-info__vehicle">2018 Subaru Impreza</p>
+          <p className="customer-info__vehicle">2018 Ferrari F1</p>
         </div>
       </div>
       <CustomerFiles />
--- client-manuspec/src/pages/Profile/Profile.jsx
+++ client-manuspec/src/pages/Profile/Profile.jsx
@@ -10,11 +10,12 @@ function Profile() {
           <p className="profile__name">Ian Pinili</p>
         </div>
         <div className="profile__middle">
-          <p className="profile__number">11975</p>
+          <p className="profile__num">778-855-3645</p>
           <p className="profile__email">gianpinili@gmail.com</p>
         </div>
         <div className="profile__right">
-          <p className="profile__job">Red Seal Automotive Technician</p>
+          <p className="profile__number">Tech #: 11975</p>
+          <p className="profile__job">Automotive Technician</p>
         </div>
       </div>
     </>
